c212c8b4cecd283e5c76f2185bfa6ba7dfba95f4
Xu,Anhua <xuanhuapku@163.com>
2024-10-22 14:33:23 +0800

    Fix typo in scrits/run_text_to_jsonstrip.sh

--- scripts/run_text_to_jsonstrip.sh
+++ scripts/run_text_to_jsonstrip.sh
@@ -4,4 +4,4 @@ echo "Make sure you run this script from the project home directory"
 
 export PYTHONPATH=./:$PYTHONPATH
 
-python tasks/text_to_json_trip.py   --input-file data/text_to_jsontrip.txt   --output-file  data/result-of-jsontrip.jsonv
+python tasks/text_to_json_trip.py   --input-file data/text_to_jsontrip.txt   --output-file  data/result-of-jsontrip.json
